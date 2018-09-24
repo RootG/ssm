e8fd49bf108fe66aff4cbf5b5093b62078def514
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-09-24 18:56:24 +0300

    Removed unnecessary folders, updated .gitignore.

--- src/main/java/ssm/ParameterStore.java
+++ src/main/java/ssm/ParameterStore.java
@@ -1,10 +1,27 @@
 package ssm;
 
 public interface ParameterStore {
+    /**
+     * Returns the value of the parameter by using default environment for it's environment.
+     *
+     * @param name The name of the parameter.
+     * @return The value.
+     */
     String getValue(String name);
 
+    /**
+     * Returns the value of the parameter by using the given environment for it's environment.
+     *
+     * @param name        The name of the parameter.
+     * @param environment The environment of the parameter.
+     * @return The value
+     */
     String getValue(String name, Environment environment);
 
+    /**
+     * Sets the default environment for this {@code ParameterStore} object. SAn invocation
+     * @return
+     */
     Environment getDefaultEnvironment();
 
     void setDefaultEnvironment(Environment defaultEnvironment);
