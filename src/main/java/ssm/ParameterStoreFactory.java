af5602a64ff7e76420981805652294e7ed5c1511
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-10-18 21:56:32 +0300

    Simplified a code block, added singleton method to factory.

--- src/main/java/ssm/ParameterStoreFactory.java
+++ src/main/java/ssm/ParameterStoreFactory.java
@@ -10,6 +10,7 @@ import java.io.UncheckedIOException;
 
 public class ParameterStoreFactory {
     private static final Logger LOGGER = LogManager.getLogger(ParameterStoreFactory.class);
+    private static ParameterStore singleton;
 
     public static ParameterStore create() {
         try {
@@ -28,21 +29,15 @@ public class ParameterStoreFactory {
     }
 
     public static ParameterStore create(Environment environment) {
-        try {
-            ParameterStoreAdapter parameterStoreAdapter = new ParameterStoreAdapter();
-            parameterStoreAdapter.setDefaultEnvironment(environment);
-            return parameterStoreAdapter;
-        } catch (SdkClientException e) {
-            LOGGER.warn("AWS Parameter Store Exception: " + e.getMessage());
+        ParameterStore parameterStore = create();
+        parameterStore.setDefaultEnvironment(environment);
+        return parameterStore;
+    }
+
+    public synchronized static ParameterStore getSingleton() {
+        if (singleton == null) {
+            singleton = create();
         }
-        LOGGER.warn("Initializing parameter store from configuration file.");
-        InputStream inputStream = ConfigurationFile.class.getClassLoader().getResourceAsStream("ParameterStore.json");
-        if (inputStream == null) {
-            throw new UncheckedIOException(
-                    new FileNotFoundException("\"ParameterStore.json\" file can not be found in resources."));
-        }
-        ConfigurationFile configurationFile = new ConfigurationFile(inputStream);
-        configurationFile.setDefaultEnvironment(environment);
-        return configurationFile;
+        return singleton;
     }
 }
